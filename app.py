621a1c381ad4acdbbb4bf6e835dcb89ae71948bd
Monish Kumar K S <86783225+Monish3004@users.noreply.github.com>
2023-09-09 12:45:54 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import matplotlib.pyplot as py
+
+st.title("This is a Streamlit Application")
+
+st.title("This is a test deployment app.")
+
+st.title("For further details : check out this [blog](https://dev.to/)")
